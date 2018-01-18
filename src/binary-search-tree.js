c9f19a1746a64e01b6fedb1b214cd8da94113feb
Nikhil Kamineni <nikhil@nk-mbp.local>
2018-01-18 17:38:19 -0500

    completed 'insert' and 'contains' in binary-search-tree.js (during demo with Sean)

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -13,16 +13,34 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-    // create new BinarySearchTree
-    // iterate over subtrees while checking if value is +/- to find out which path to go down
-    // find the subtree that returns undefined as 
-    // assign new BinarySearchTree to be either this.left or this.right value
-
+    const newNode = new BinarySearchTree(value);
+    if (value < this.value) {
+      if (!this.left) {
+        this.left = newNode;
+      } else {
+        this.left.insert(value)
+      }
+    } else {
+      if (!this.right) {
+        this.right = newNode;
+      } else {
+        this.right.insert(value);
+      }
+    }
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-
+    if (this.value === target) {
+      return true;
+    }
+    if (this.left) {
+      if (this.left.contains(target)) return true;
+    }
+    if (this.right) {
+      if (this.right.contains(target)) return true;
+    }
+    return false;
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
